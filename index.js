79b677bc8605a26656179d62558db7f3caa9c2a8
Lalita Desouza <lmdesou@yahoo.com>
2017-11-14 19:58:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
 var newTheBeatlesPlay = [];
-  for (i = 0; i = musicians.length; i++) {
+  for (i = musicians.length; i++) {
     newTheBeatlesPlay.push(`${musicians} plays ${instruments}`)
   }
   return newTheBeatlesPlay
